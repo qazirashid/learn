11b9d36aafa2f9a84c4b929615a6fe5848ead89a
qazirashid <qazirashid@gmail.com>
2019-07-30 19:34:29 -0700

    Some enhancements to datastructure for Sudoku Grid

--- diagram/sudoku/test.c
+++ diagram/sudoku/test.c
@@ -3,72 +3,72 @@
 #define GBASE  3    //Base of the Grid. size of boxes in the grid.
 #define GSIZE  GBASE*GBASE   // Number of rows and colums
 
+/*************** Grid Data Structure ************/
+typedef struct grid_t{
+	int Status[GSIZE][GSIZE];
+	int Output[GSIZE][GSIZE];
+	int Choice[GSIZE][GSIZE][GSIZE];
+} Grid;
+
 /********* Function Declarations *************/
 void printMatrix(int m[GSIZE][GSIZE]);
-int initGrid(int Status[GSIZE][GSIZE], int Output[GSIZE][GSIZE], int Choice[GSIZE][GSIZE][GSIZE]);
+void printGrid(Grid g);
+int initGrid(Grid* gp);
 void printChoice( int Choice[GSIZE][GSIZE][GSIZE]);
-int canContain(int i, int j, int v, int Choice[GSIZE][GSIZE][GSIZE]);
-int setCellValue(const int i, const int j, const int v, int Status[GSIZE][GSIZE], int Output[GSIZE][GSIZE], int Choice[GSIZE][GSIZE][GSIZE]);
+int canContain(Grid g, int i, int j, int v);
+int setCellValue(Grid* gp, const int i, const int j, const int v);
 int lowerBlockBound(int i);
 int upperBlockBound(int i);
-int setCellAndPrint(int i, int j, int v, int Status[GSIZE][GSIZE], int Output[GSIZE][GSIZE], int Choice[GSIZE][GSIZE][GSIZE]);
+int setCellAndPrint(Grid* gp, int i, int j, int value);
+
 /**************** Main *************************/
 
 int main (void){
-	int Status[GSIZE][GSIZE]; 
-	// Status(i,j)=1 of grid indicates that Cell(i,j) has been set to some value. 
-	// Status(i,j)=0 means that Cell(i,j) is unknown yet.
-	int Output[GSIZE][GSIZE]; 
-	// Output[i][j] = value, indicates that Cell[i][j]=value. Only meaningful if Status(i,j)=1.
-	// Output[i][j] = 0, indicates that Cell[i][j] is currently unknown and has multiple legal values.
-	int Choice[GSIZE][GSIZE][GSIZE];
-	// Choice[i][j] points to an array[GSIZE] of ints that contains all legal values allowed in Cell[i][j].
-	//for(int i=0; i<GSIZE; i++)
-	       	//for(int j=0; j<GSIZE; j++){
-	       	//Choice[i][j] = malloc(GSIZE*sizeof(int));
-	     //}
-	//Allocated memory for all arrays of choices. Could not find a way to allocate it statically in a for loop. Maybe there is a way, don't know. 
-	//
+	Grid g;
+	Grid* gp = &g;
 	//Initialize Grid with all 0s.
-	initGrid(Status, Output, Choice);		
-	setCellValue(0,0,1, Status, Output, Choice);
-	setCellValue(3,1,1, Status, Output, Choice) ;
-	setCellValue(7,2,1, Status, Output, Choice);
-	setCellValue(1,3,1, Status, Output, Choice);
-	setCellValue(2,7,1, Status, Output, Choice);
-	setCellValue(0,1,8, Status, Output, Choice);
-	setCellAndPrint(3,2,8, Status, Output, Choice);
-
+	initGrid(gp);		
+	printGrid(g);
+	setCellValue(gp,0,0,1);
+	setCellValue(gp,3,1,1) ;
+	setCellValue(gp,7,2,1);
+	setCellValue(gp,1,3,1);
+	setCellValue(gp,2,7,1);
+	setCellAndPrint(gp, 3,2,2);
+	printGrid(g);
 }
 /***************** Helper Functions ***********************/
-int setCellAndPrint(int i, int j, int v, int Status[GSIZE][GSIZE], int Output[GSIZE][GSIZE], int Choice[GSIZE][GSIZE][GSIZE]){
-
-	if (setCellValue(i,j,v,Status,Output,Choice)){
+void printGrid(Grid g){
 		printf("Current Status is:\n");
-		printMatrix(Status);
+		printMatrix(g.Status);
 		printf("Current Output is:\n");
-		printMatrix(Output);
+		printMatrix(g.Output);
 		printf("Current Legal Choices are:\n");
-		printChoice(Choice);
-		return(0);
-	}else printf("Failed to Set Cell(%d, %d) to %d \n", i, j, v);
+		printChoice(g.Choice);
+	}
+
+int setCellAndPrint(Grid* gp, int i, int j, int v){
+	if (setCellValue(gp, i,j,v)){
+		printGrid(*gp);	
+	}else 
+		printf("Failed to Set Cell(%d, %d) to %d \n", i, j, v);
 	return(1);
 }
 
-int initGrid(int Status[GSIZE][GSIZE], int Output[GSIZE][GSIZE], int Choice[GSIZE][GSIZE][GSIZE]){
-	int i=0, j=0;
-	for(int i=0; i<GSIZE; i++)
+int initGrid(Grid* gp){ 
+	int i=0, j=0, k=0;
+	for(i=0; i<GSIZE; i++)
 	       for(j=0; j<GSIZE; j++){
-	       	Status[i][j] = 0;
+	       	gp->Status[i][j] = 0;
 	      }
 	for(i=0; i<GSIZE; i++)
 	       for(j=0; j<GSIZE; j++){
-	       	Output[i][j] = 0;
+	       	gp->Output[i][j] = 0;
 	      }
 	for(i=0; i<GSIZE; i++)
 	       for(j=0; j<GSIZE; j++){
-		       for(int k=0; k< GSIZE; k++)
-			       Choice[i][j][k] = k; 
+		       for(k=0; k< GSIZE; k++)
+			       gp->Choice[i][j][k] = k; 
 	      }
 	return(1); // Sucesss
 
@@ -103,34 +103,37 @@ void printChoice( int Choice[GSIZE][GSIZE][GSIZE]){
 }
 
 
-int setCellValue(const int i, const int j, const int v, int Status[GSIZE][GSIZE], int Output[GSIZE][GSIZE], int Choice[GSIZE][GSIZE][GSIZE]){
+int setCellValue(Grid* gp, const int i, const int j, const int v){
 	// will attempt to set cell(i,j) value to v. On Success, returns 1. On failure returns 0. 
 	// Failure occurs when the v is illegal in Cell(i,j).
 
-	if((v<0) || (v >=GSIZE))
+	if((v<0) || (v >=GSIZE)){
+		printf("setCellValue():invalid v was passed to setCellValue()");
 		return(0); // invalid v was passed.
-	if(canContain(i,j,v, Choice)){
+
+	}
+	if(canContain((*gp), i,j,v)){
 		// If v is legal in Cell(i,j), Then do all of the below
 		// 1- set status of Cell(i,j)=1. this indicates that Cell has been occupied.
-		Status[i][j] = 1;
+		gp->Status[i][j] = 1;
 		// 2- set output of Cell(i,j)=v+1 (1 added because of C-index starting at 0. This indicates the output user will get. 
-		Output[i][j] = v;
+		gp->Output[i][j] = v;
 		// 3- Set all of Choice[i][j][k] for (0<= k < GSIZE) to -1. This indicates that Cell is occupied. Next attempt to set it to some value will fail.
 		for(int kr=0; kr<GSIZE;kr++)
-			Choice[i][j][kr] = -1;
+			gp->Choice[i][j][kr] = -1;
 		//4- For all Cells in the row i (0<=i<GSIZE), exclude v by setting Choice[i][j][v]=-1, because no further Cell in this row can be set to v.
 		for(int ir=0; ir<GSIZE;ir++)
-			Choice[ir][j][v] = -1;
+			gp->Choice[ir][j][v] = -1;
 		//5- For all Cells in the column j (0<=j<GSIZE), exclude v by setting Choice[i][j][v]=-1, because no further Cell in the column can be set to v.
 		for(int jr=0; jr<GSIZE; jr++)
-			Choice[i][jr][v] = -1;
+			gp->Choice[i][jr][v] = -1;
 		//6- For all Cells in the 3x3 block containing Cell(i,j), exclude v by setting Choice[b][c][v] ==0, because this block cannot contain v in any other Cell.
 		// This is 2-D Loop. Calculate lower and upper bounds to contain (i,j) in the 3x3 box containing Cell (i,j)
 		int lbbi = lowerBlockBound(i); int ubbi = upperBlockBound(i);
 		int lbbj = lowerBlockBound(j); int ubbj = upperBlockBound(j);
 		for(int ir=lbbi; ir<ubbi; ir++)
 			for(int jr=lbbj; jr<ubbj; jr++)
-				Choice[ir][jr][v]=-1;
+				gp->Choice[ir][jr][v]=-1;
 		// Trying to set any other cell in this block to v will fail in future.
 
 		//ALL DONE. Ready to declare success.
@@ -141,12 +144,13 @@ int setCellValue(const int i, const int j, const int v, int Status[GSIZE][GSIZE]
 	return(0);
 }
 
-int canContain(int i, int j, int v, int Choice[GSIZE][GSIZE][GSIZE]){
+int canContain(Grid g, int i, int j, int v){
 	// Tests whether Cell(i,j) can contain v. 
 	// returns 1 if True. 0 if False.
 	for(int k=0; k<GSIZE; k++){
-		if(Choice[i][j][k] == v) return(1);
+		if(g.Choice[i][j][k] == v) return(1);
 	}
+	printf("canContain():Cell(%d,%d) cannot contain %d\n", i,j,v);
 	return(0);
 }
 int lowerBlockBound(int i){
